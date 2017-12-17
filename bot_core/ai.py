53113d4f4d82e4d2cf22b5c006f77041fe0ad1d3
DarthSergeant <darthsergeant@gmail.com>
2017-12-17 15:24:01 -0500

    Update ai.py

--- bot_core/ai.py
+++ bot_core/ai.py
@@ -1,7 +1,7 @@
 """##########################
 # Imports
 ##########################"""
-import echo.echo.py
+from echo.echo import echo_response
 
 """##########################
 # Bot Core
@@ -9,6 +9,6 @@ import echo.echo.py
 #create a function to determine which database to read from
 def post_response(text):
   if echo(text): 
-     message = (echo(text))
+     message = (echo_response(text))
      return message
 
