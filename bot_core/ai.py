d9e0c7a8e05991ccb17f9aa4990d6b2ec97b8ebf
DarthSergeant <darthsergeant@gmail.com>
2017-12-17 13:01:07 -0500

    Update ai.py

--- bot_core/ai.py
+++ bot_core/ai.py
@@ -1,1 +1,15 @@
+"""##########################
+# Imports
+##########################"""
+import echo.echo.py
+
+"""##########################
+# Bot Core
+##########################"""
+#create a function to determine which database to read from
+def post_response(text):
+ # if "cat" in text:
+  #   return("cat")
+                  
+   return(export_hrl_response(text))
 
